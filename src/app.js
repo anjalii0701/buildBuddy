2a666bb146123eab3cf2cc73ddc0eb400f2c06be
anjalii0701 <anjalisharma288863@gmail.com>
2026-02-26 02:32:27 +0530

    adding validations

--- src/app.js
+++ src/app.js
@@ -59,8 +59,11 @@ app.patch("/user",async(req,res)=>{
     const userId= req.body.userId;
     const data=req.body;
     try{
-        await User.findByIdAndUpdate({_id:userId },data);
+        await User.findByIdAndUpdate({_id:userId },data,{
+            runValidators:true,
+        });
         res.send("User Updated successfully");
+       
     }catch(err){
         res.status(400).send("Something went wrong");
     }
--- src/models/user.js
+++ src/models/user.js
@@ -2,33 +2,51 @@ const mongoose = require("mongoose");
 
 const userSchema= new mongoose.Schema({
     firstName:  {
-        type: String
+        type: String,
+        required:true,
+        minLength:4,
+        maxLength:50,
     },
     lastName: {
         type: String
     } ,
     emailId: {
-        type: String
+        type: String,
+        required:true,
+        lowercase:true,
+        unique:true,
+        trim:true,
+
     },
     password: {
-        type: String
+        type: String,
+        required:true, 
     },
-    // age:{
-    //     type: Number
-    // },
-    // gender:{
-    //     type: String
-    // },
-    // photoUrl:{
-    //     type:String
-    // },
-    // skills:{
-    //     type:[String ]
-    // },
-    // about:{
-    //     type:String ,
-    //     default:"This is default about the user"
-    // }
-});
+    age:{
+        type: Number,
+        min:18,
+    },
+    gender:{
+        type: String,
+        validate(value){
+            if(!["male" ,"female","other"].includes(value)){
+                throw new Error("Gender data is not valid")
+            }
+        }
+    },
+    photoUrl:{
+        type:String
+    },
+    skills:{
+        type:[String ]  
+    },
+    about:{
+        type:String ,
+        default:"This is default about the user"
+    }
+},{
+    timestamps:true,
+}
+);
   
 module.exports=mongoose.model("User",userSchema);
