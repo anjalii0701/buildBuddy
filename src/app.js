bb62065d9004c22056c7d983b27aca0ac859aa31
anjalii0701 <anjalisharma288863@gmail.com>
2026-02-27 22:21:49 +0530

    API level data sanitization

--- src/app.js
+++ src/app.js
@@ -59,7 +59,25 @@ app.patch("/user",async(req,res)=>{
     const userId= req.body.userId;
     const data=req.body;
     try{
-        await User.findByIdAndUpdate({_id:userId },data,{
+    const ALLOWED_UPDATES=[
+        "userId" 
+        ,"photoUrl"
+        ,"about"
+        ,"gender"
+        ,"age",
+        "skills"
+    ]
+    const isUpdateAllowed=Object.keys(data).every((k)=>
+        ALLOWED_UPDATES.includes(k)
+    );
+    if(!isUpdateAllowed){
+        throw new Error("Update not allowed");
+    }
+    if(data?.skills.length>10){
+        throw new Error("Skills cannot be more than 10");
+    }
+    
+     const user=   await User.findByIdAndUpdate({_id:userId },data,{
             runValidators:true,
         });
         res.send("User Updated successfully");
